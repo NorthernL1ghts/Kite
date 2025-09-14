df0688952e0c26fb5a0a7c0663e56610dc58e217
NorthernL1ghts <noreply@local>
2025-09-14 16:01:15 +0100

    Began Vulkan Renderer front/backend scaffolding

--- code/engine/src/core/application.c
+++ code/engine/src/core/application.c
@@ -9,6 +9,8 @@
 #include "core/input.h"
 #include "core/clock.h"
 
+#include "renderer/renderer_frontend.h"
+
 typedef struct application_state {
     game* game_inst;
     b8 is_running;
@@ -69,6 +71,12 @@ b8 application_create(game* game_inst) {
         return FALSE;
     }
 
+    // Renderer startup
+    if (!renderer_initialize(game_inst->app_config.name, &app_state.platform)) {
+        KFATAL("Failed to initialize renderer. Aborting application.");
+        return FALSE;
+    }
+
     // Initialize the game.
     if (!app_state.game_inst->initialize(app_state.game_inst)) {
         KFATAL("Game failed to initialize.");
@@ -117,6 +125,11 @@ b8 application_run() {
                 break;
             }
 
+            // TODO: refactor packet creation
+            render_packet packet;
+            packet.delta_time = delta;
+            renderer_draw_frame(&packet);
+
             // Figure out how long the frame took and, if below
             f64 frame_end_time = platform_get_absolute_time();
             f64 frame_elapsed_time = frame_end_time - frame_start_time;
@@ -154,6 +167,8 @@ b8 application_run() {
     event_unregister(EVENT_CODE_KEY_RELEASED, 0, application_on_key);
     event_shutdown();
     input_shutdown();
+
+    renderer_shutdown();
 
     platform_shutdown(&app_state.platform);
 
