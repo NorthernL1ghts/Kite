33538251742ee6d443f57cf5b0f6b43af2ba0177
NorthernL1ghts <noreply@local>
2025-09-21 11:13:00 +0100

    Vulkan Graphics Pipeline Asset Setup

--- engine/src/math/kmath.c
+++ engine/src/math/kmath.c
@@ -9,7 +9,7 @@ static b8 rand_seeded = FALSE;
 /**
  * Note that these are here in order to prevent having to import the
  * entire <math.h> everywhere.
- */
+*/
 f32 ksin(f32 x) {
     return sinf(x);
 }
